8d58da2c9034c22dc485846ffe36c13f81a350ac
meryjean18 <merybettavilapoma@gmail.com>
2022-06-09 20:50:07 -0500

    cambios

--- Examen.Logic/ExamenBL.cs
+++ Examen.Logic/ExamenBL.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Examen.Core;
+using Examen.Data;
 
 namespace Examen.Logic
 {
     public class ExamenBL
     {
+        public static List<Prestamo> Listar()
+        {
+            using (var db = new FinancieraEntities())
+            {
+                return db.Prestamo.ToList();
+            } 
+        }
 
+        public static Cliente Buscar(int id)
+        {
+            using (var db = new FinancieraEntities())
+            {
+                return db.Cliente.Find(id);
+            }
+        }
     }
 }
